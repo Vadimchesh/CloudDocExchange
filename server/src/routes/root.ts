5fcba06cbb354c181aa40a3be38887f5f74d43b0
Vadimchesh <vadimlindo@gmail.com>
2024-01-17 22:04:07 +0100

    be: init server

---
+++ server/src/routes/root.ts
@@ -0,0 +1,12 @@
+import { FastifyPluginAsync } from "fastify";
+
+const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get("/", async function (request, reply) {
+    return { root: true };
+  });
+  fastify.post("/file", async function (request, reply) {
+    return { fileName: "sample.pdf" };
+  });
+};
+
+export default root;
