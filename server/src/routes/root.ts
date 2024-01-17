81513ed75540748021ddcb51685282aaa46a2272
Vadimchesh <vadimlindo@gmail.com>
2024-01-17 22:28:07 +0100

    be: mock files route

--- server/src/routes/root.ts
+++ server/src/routes/root.ts
@@ -4,8 +4,29 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
     return { root: true };
   });
-  fastify.post("/file", async function (request, reply) {
+  fastify.post("/api/files", async function (request, reply) {
     return { fileName: "sample.pdf" };
+  });
+  fastify.get("/api/files", async function (request, reply) {
+    return {
+      files: [
+        {
+          id: "1",
+          name: "file.pdf",
+          url: "http://s3.bucket.com/file.pdf",
+        },
+        {
+          id: "2",
+          name: "file2.pdf",
+          url: "http://s3.bucket.com/file2.pdf",
+        },
+        {
+          id: "3",
+          name: "file3.pdf",
+          url: "http://s3.bucket.com/file3.pdf",
+        },
+      ],
+    };
   });
 };
 
